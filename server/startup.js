33ec1ab757d55520bee636c8c6693ae4ed681736
Alfonso <adiazalo@protonmail.com>
2016-11-12 20:43:33 -0800

    Unauthorized page added + user roles

--- server/startup.js
+++ server/startup.js
@@ -7,5 +7,11 @@ Meteor.startup(function()[
     user.profile.firstname = options.firstname;
     user.profile.lastname = options.lastname;
     return user;
-  })
+  });
+  var users = Meteor.users.find().fetch();
+  _.each(users,function(userData){
+    if (userData.emails[0].address === 'george@home.com'){
+      Roles.addUsersToRoles(userData,['admin']);
+    }
+  });
 ]);

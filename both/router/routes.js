ca3de50516be0f80137405bce8c3aa79a5ea2da0
Alfonso Diaz <adiazalo@users.noreply.github.com>
2016-11-19 22:29:23 -0800

    Update routes.js

--- both/router/routes.js
+++ both/router/routes.js
@@ -1,6 +1,7 @@
 FlowRouter.route(['/','/home'],{ // two choices for route
     subscriptions:function(){
     this.register('categorieslist',Meteor.subscribe('category'));
+    this.register('productsList',Meteor.subscribe('products'));
   },
   action:function(){
     console.log("Running Action to render templates into layouts");
@@ -11,6 +12,7 @@ FlowRouter.route(['/','/home'],{ // two choices for route
 FlowRouter.route('/admin',{
     subscriptions:function(){
     this.register('categorieslist',Meteor.subscribe('category'));
+    this.register('productsList',Meteor.subscribe('products'));
   },
   action:function(){
     if(Roles.userIsInRole(Meteor.userId(),'admin')){

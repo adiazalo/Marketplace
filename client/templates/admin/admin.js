0a646e9f3f7cac928f2e615ea350c4d9c4d371bc
Alfonso Diaz <adiazalo@users.noreply.github.com>
2016-11-19 22:37:26 -0800

    Adding product changes

--- client/templates/admin/admin.js
+++ client/templates/admin/admin.js
@@ -6,3 +6,16 @@ Template.admin.helpers({
     return Product.find();
   }
 });
+  
+Template.admin.events({
+  'click .addProduct':function(evt,tmpl){
+    var product = {};
+    product.name = tmpl.find('#productname').value;
+    product.categoryId = tmpl.find('#category').value;
+    product.description = tmpl.find('#description').value;
+    product.price = tmpl.find('#price').value;
+    product.rating = tmpl.find('#rating').value;
+    Meteor.call('Product.insert',product,function(){
+    })
+  }
+});
